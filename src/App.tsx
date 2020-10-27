778d06b244a70984af69879bfc839a5460ae4fcc
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-10-27 23:59:47 +0100

    Removed unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
 
 import { Client } from './components/Client/Client';
-import logo from './logo.svg';
 import './App.css';
 
 export const App: React.FunctionComponent<{}> = () => {
   return (
     <div className="App" data-testid="app">
       <Client />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
