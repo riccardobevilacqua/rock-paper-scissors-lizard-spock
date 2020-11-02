08a1395c95f54683fb029f964474f8466fadef4e
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-11-02 14:11:23 +0100

    Updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { Client } from './components/Client/Client';
-import './App.css';
 
 export const App: React.FunctionComponent<{}> = () => {
   return (
-    <div className="App" data-testid="app">
-      <Client />
-    </div>
+    <section className="section" data-testid="app">
+      <div className="container">
+        <Client />
+      </div>
+    </section>
   );
 }
