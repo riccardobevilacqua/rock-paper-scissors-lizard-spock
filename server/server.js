662db3961d19863f92c2b5447b6562442f3ab78d
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-11-02 22:22:14 +0100

    Updated server

--- server/server.js
+++ server/server.js
@@ -11,55 +11,81 @@ const server = app.listen(port, () => {
 });
 
 const io = socket(server);
-const victoryThreshold = 3;
+const victoryThreshold = 2;
 let scoreBoard = [];
 let currentSelections = [];
 
 io.on('connection', function (socket) {
   socket.on('joinServer', function (payload) {
-    const { userId } = payload;
-    socket.userId = userId;
-    scoreBoard.push({
-      userId,
-      score: 0,
-      isWinner: false
-    });
-    io.emit('joinServer', scoreBoard);
-    console.log(`Player-${userId} joined.`);
-  });
-
-  socket.on('setSelection', function (payload) {
-    if (!!payload) {
-      const { userId, selection } = payload;
-
-      currentSelections.push({
-        userId,
-        selection,
-        score: 0
-      });
-
-      if (scoreBoard.length === currentSelections.length) {
-        scoreBoard = updateScoreBoard({
-          currentSelections,
-          scoreBoard,
+    try {
+      if (payload && payload.userId) {
+        const { userId } = payload;
+        socket.userId = userId;
+        scoreBoard.push({
+          userId,
+          score: 0,
+          isWinner: false
         });
-        const winner = scoreBoard[0].score >= victoryThreshold ? scoreBoard[0].userId : null;
-        io.emit('endRound', {
-          currentSelections,
-          scoreBoard,
-          winner,
-        });
-
-        currentSelections = [];
+        io.emit('joinServer', scoreBoard);
+        console.log(`Player-${userId} joined.`);
       }
-
-      console.log(`Player-${userId} selected ${selection}`);
+    } catch (err) {
+      console.log(err);
     }
   });
 
+  socket.on('setSelection', function (payload) {
+    try {
+      if (payload && payload.userId && payload.selection) {
+        const { userId, selection } = payload;
+
+        currentSelections.push({
+          userId,
+          selection,
+          score: 0
+        });
+
+        if (scoreBoard.length === currentSelections.length) {
+          scoreBoard = updateScoreBoard({
+            currentSelections,
+            scoreBoard,
+          });
+          const winner = scoreBoard[0].score >= victoryThreshold ? scoreBoard[0].userId : null;
+          io.emit('endRound', {
+            currentSelections,
+            scoreBoard,
+            winner,
+          });
+
+          currentSelections = [];
+        }
+
+        console.log(`Player-${userId} selected ${selection}`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  socket.on('playAgain', function () {
+    scoreBoard = [...scoreBoard].map(item => {
+      if (item) {
+        item.score = 0;
+        item.isWinner = false;
+      }
+    });
+    currentSelections = [];
+    winner = null;
+    io.emit('restartGame', { scoreBoard });
+  });
+
   socket.on('disconnect', function () {
-    scoreBoard = [...scoreBoard].filter(item => item.userId !== socket.userId);
-    io.emit('leaveServer', socket.userId);
-    console.log(`Player-${socket.userId} left.`);
+    try {
+      scoreBoard = [...scoreBoard].filter(item => item && item.userId !== socket.userId);
+      io.emit('leaveServer', socket.userId);
+      console.log(`Player-${socket.userId} left.`);
+    } catch (err) {
+      console.log(err);
+    }
   });
 });
